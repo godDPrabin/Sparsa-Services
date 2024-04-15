87e55f47293ef6ffebf2a505743fa1f68c0bf4b7
dhunganaPradeep <pradeep.dhungana23@gmail.com>
2024-04-16 00:55:45 +0545

    call us

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+    "use strict";
+
+    var winH = $(window).height();
+
+    $("header").height(winH);
+
+    $("header .container > div").css(
+      "top",
+      winH / 2 - $("header .container > div").height() / 2
+    );
+    $(".navbar ul.navbar-nav li a").on("click", function (e) {
+      var getAttr = $(this).attr("href");
+
+      e.preventDefault();
+      $("html").animate({ scrollTop: $(getAttr).offset().top }, 1000);
+    });
+  });
+  
+// Call Us ========================================
+document.addEventListener('DOMContentLoaded', function() {
+    var callButton = document.querySelector('.call-us-btn');
+    callButton.addEventListener('click', function(event) {
+      // Check if the device is not a phone
+      if (!navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) {
+        // Prevent default 'tel:' link behavior
+        event.preventDefault();
+        // Display the number in an alert or any other way you prefer
+        alert('Call us at: 61-488-900-468');
+      }
+    });
+  });
